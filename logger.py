20cb87a00fbef59b3e2b891e774876c396f38303
nickhung <nick@microsys.com.tw>
2025-09-08 14:28:32 +0800

    feat:
    1. logger 加上 force

--- logger.py
+++ logger.py
@@ -3,7 +3,8 @@ import logging
 def get_logger(name: str):
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s - %(message)s"
+        format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
+        force=True # 確保不會被其他地方的 basicConfig 蓋掉
     )
     return logging.getLogger(name)
 
